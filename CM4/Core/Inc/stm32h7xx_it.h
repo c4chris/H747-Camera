882501e92cc0ff0c4d966228ae785b8600d1a70f
Christian Iseli <christian.iseli@epfl.ch>
2022-02-14 02:11:13 +0100

    Setup TOUCH_INT as interrupt pin

--- CM4/Core/Inc/stm32h7xx_it.h
+++ CM4/Core/Inc/stm32h7xx_it.h
@@ -54,6 +54,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void DMA1_Stream0_IRQHandler(void);
 void DMA1_Stream1_IRQHandler(void);
+void EXTI9_5_IRQHandler(void);
 void USART1_IRQHandler(void);
 void TIM7_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
--- CM4/Core/Src/stm32h7xx_it.c
+++ CM4/Core/Src/stm32h7xx_it.c
@@ -193,6 +193,20 @@ void DMA1_Stream1_IRQHandler(void)
 }
 
 /**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(TOUCH_INT_Pin);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
   * @brief This function handles USART1 global interrupt.
   */
 void USART1_IRQHandler(void)
